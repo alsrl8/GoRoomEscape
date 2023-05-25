ebc5f8d6b456ccd8533680f3815f01b6ec2b254c
mingi.song <mingi.song@brique.co.kr>
2023-05-25 18:55:38 +0900

    아이템 수정

--- game/data/item.go
+++ game/data/item.go
@@ -32,7 +32,6 @@ func GetItemPositionAndType() *[]structure.ItemPositionAndType {
 	return &[]structure.ItemPositionAndType{
 		{RoomPosition: structure.Position{Row: 0, Col: 4}, ItemType: constants.Hammer},
 		{RoomPosition: structure.Position{Row: 0, Col: 4}, ItemType: constants.Box},
-		{RoomPosition: structure.Position{Row: 2, Col: 1}, ItemType: constants.Key},
 		{RoomPosition: structure.Position{Row: 3, Col: 3}, ItemType: constants.Box},
 		{RoomPosition: structure.Position{Row: 3, Col: 6}, ItemType: constants.HealPotion},
 		{RoomPosition: structure.Position{Row: 3, Col: 9}, ItemType: constants.Box},
